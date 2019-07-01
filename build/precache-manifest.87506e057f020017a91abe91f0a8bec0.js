6aa06bcd96321157919020728c1ea246b3d23ef4
Redhead <stadnikov.pasha@mail.ru>
2019-07-01 17:46:41 +0300

    final

---
+++ build/precache-manifest.87506e057f020017a91abe91f0a8bec0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8cf08d09c12be3ddd43285dd7da26b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "997f49c86b06842c7a8e",
+    "url": "/static/css/main.66d0e4f2.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "997f49c86b06842c7a8e",
+    "url": "/static/js/main.8887893f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "04efc48bd35287fa1222bdbd1d1861aa",
+    "url": "/static/media/bomb.04efc48b.svg"
+  },
+  {
+    "revision": "35d811aab9bec67464a0e4555fc8097e",
+    "url": "/static/media/flag.35d811aa.svg"
+  }
+]);
